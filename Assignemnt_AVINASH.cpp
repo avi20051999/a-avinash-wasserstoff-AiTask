1b3c604723f7544fd43795a1c0d59cd57a4e6e98
avi20051999 <36727499+avi20051999@users.noreply.github.com>
2022-03-29 20:11:24 +0530

    Update Assignemnt_AVINASH.cpp
    Added a unit test case

--- Assignemnt_AVINASH.cpp
+++ Assignemnt_AVINASH.cpp
@@ -1,81 +1,80 @@
-#include <bits/stdc++.h>
+#include<string>
+
+#include<vector>
+
+#include<map>
+
+#include<iostream>
+
+#include<fstream>
+
 using namespace std;
-
-struct Date
-{
-    int days, month, year;
+using std::ifstream;
+struct Date {
+  int days, month, year;
 };
-int main()
-{
-int numberOfOrders;
-cin>>numberOfOrders;
-    map<string,int> productFrequency;
-string productName;
-int productCount;
-Date orderedAgo;
-string maximumOrderedProduct="", secondMaximumOrderedProduct="";
-for(int orderId=0;orderId<numberOfOrders;++orderId)
-{
-   int numberOfProducts = 0;
-   cin>>numberOfProducts;
-   cin>>orderedAgo.days;
-   cin>>orderedAgo.month;
-   cin>>orderedAgo.year;
-   for(int productId=0;productId<numberOfProducts;++productId)
-   {
-       cin>>productName;
-       cin>>productCount;
-       
-       if(orderedAgo.days<=30 && orderedAgo.month==0 && orderedAgo.year==0)
-       {
-           productFrequency[productName]+=productCount;
+vector < string > processing(string file) {
 
-           if(maximumOrderedProduct == "")
-           {
-               maximumOrderedProduct = productName;
-           }
-           else if(secondMaximumOrderedProduct == "")
-           {
-               if(productFrequency[productName] > productFrequency[maximumOrderedProduct])
-               {
-                   secondMaximumOrderedProduct = maximumOrderedProduct;
-                   maximumOrderedProduct = productName;
-               }
-               else
-               {
-                   secondMaximumOrderedProduct = productName;
-               }
-           }
-           else
-           {
-               if(productFrequency[maximumOrderedProduct] < productFrequency[productName])
-               {
-                        secondMaximumOrderedProduct = maximumOrderedProduct;
-                        maximumOrderedProduct = productName;
-                    }
-                    else if(productFrequency[secondMaximumOrderedProduct] < productFrequency[productName])
-                    {
-                        secondMaximumOrderedProduct = productName;
-                    }
-           }
-       }
-   }
+  ifstream indata;
+  indata.open(file);
+  int numberOfOrders = 0;
+
+  indata >> numberOfOrders;
+  vector < string > result;
+  map < string, int > productFrequency;
+  string productName;
+  int productCount;
+  Date orderedAgo;
+  string maximumOrderedProduct = "", secondMaximumOrderedProduct = "";
+  for (int orderId = 0; orderId < numberOfOrders; ++orderId) {
+    int numberOfProducts = 0;
+    indata >> numberOfProducts;
+    indata >> orderedAgo.days;
+    indata >> orderedAgo.month;
+    indata >> orderedAgo.year;
+    for (int productId = 0; productId < numberOfProducts; ++productId) {
+      indata >> productName;
+      indata >> productCount;
+
+      if (orderedAgo.days <= 30 && orderedAgo.month == 0 && orderedAgo.year == 0) {
+        productFrequency[productName] = productCount;
+
+        if (maximumOrderedProduct == "") {
+          maximumOrderedProduct = productName;
+        } else if (secondMaximumOrderedProduct == "") {
+          if (productFrequency[productName] > productFrequency[maximumOrderedProduct]) {
+            secondMaximumOrderedProduct = maximumOrderedProduct;
+            maximumOrderedProduct = productName;
+          } else {
+            secondMaximumOrderedProduct = productName;
+          }
+        } else {
+          if (productFrequency[maximumOrderedProduct] < productFrequency[productName]) {
+            secondMaximumOrderedProduct = maximumOrderedProduct;
+            maximumOrderedProduct = productName;
+          } else if (productFrequency[secondMaximumOrderedProduct] < productFrequency[productName]) {
+            secondMaximumOrderedProduct = productName;
+          }
+        }
+      }
+    }
+  }
+
+  if (maximumOrderedProduct == "") {
+    return result;
+  }
+  result.push_back(maximumOrderedProduct);
+  if (secondMaximumOrderedProduct == "") {
+    return result;
+  }
+  result.push_back(secondMaximumOrderedProduct);
+  return result;
+
 }
-if(maximumOrderedProduct=="")
-{
-   cout<<"There are no products";
-}
-else
-{
-cout<<"Maximum ordered product: "<<maximumOrderedProduct<<endl;
-}
-if(secondMaximumOrderedProduct=="")
-{
-   cout<<"There is only one product";
-}
-else
-{
-cout<<"Second maximum ordered product: "<<secondMaximumOrderedProduct<<endl;
-}
-return 0;
+
+int main() {
+  vector < string > output = processing("example.txt");
+  assert(output.size() == 2);
+  assert(output[0] == "Mobile");
+  assert(output[1] == "Book");
 }
